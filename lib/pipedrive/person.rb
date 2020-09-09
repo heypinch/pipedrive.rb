2032466c08ed10a567d7ca12c88256a23f239b27
Chris Raethke <chris@codesoda.com>
2020-09-09 17:03:27 +1000

    Add support for person search

--- lib/pipedrive/person.rb
+++ lib/pipedrive/person.rb
@@ -14,5 +14,14 @@ module Pipedrive
       return to_enum(:find_by_name, params) unless block_given?
       follow_pagination(:make_api_call, [:get, 'find'], params) { |item| yield item }
     end
+
+    def search(*args)
+      params = args.extract_options!
+      params[:term] ||= args[0]
+      fail 'term is missing' unless params[:term]
+#      params[:search_by_email] ||= args[1] ? 1 : 0
+      return to_enum(:search, params) unless block_given?
+      follow_pagination(:make_api_call, [:get, 'search'], params) { |item| yield item }
+    end
   end
 end
--- spec/lib/pipedrive/person_spec.rb
+++ spec/lib/pipedrive/person_spec.rb
@@ -21,4 +21,11 @@ RSpec.describe ::Pipedrive::Person do
       expect { |b| subject.find_by_name('term', &b)}.to yield_successive_args(1,2)
     end
   end
+
+  context '#search' do
+    it 'should call #make_api_call with search term' do
+      expect(subject).to receive(:make_api_call).with(:get, 'search', {term: 'term', start: 0})
+      expect { |b| subject.search('term', &b)}.to yield_successive_args
+    end
+  end
 end
