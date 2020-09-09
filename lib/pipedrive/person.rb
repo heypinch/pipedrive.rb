6ef246562bca5e54da99cbc38c3d6a7aa34a00b8
Chris Raethke <chris@codesoda.com>
2020-09-09 17:05:17 +1000

    Remove unused code

--- lib/pipedrive/person.rb
+++ lib/pipedrive/person.rb
@@ -19,7 +19,6 @@ module Pipedrive
       params = args.extract_options!
       params[:term] ||= args[0]
       fail 'term is missing' unless params[:term]
-#      params[:search_by_email] ||= args[1] ? 1 : 0
       return to_enum(:search, params) unless block_given?
       follow_pagination(:make_api_call, [:get, 'search'], params) { |item| yield item }
     end
